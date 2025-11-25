202c8afd66c47c770a03b2e0269b75f5980da32b
Trevern G. LAMONTAGNE <trevernlamontagne10@gmail.com>
2025-11-24 22:53:35 -0400

    Added css styling and started javascript for userlist

---
+++ user.php
@@ -0,0 +1,10 @@
+<?php
+    $host = 'localhost';
+    $username = 'lab5_user';
+
+
+
+
+
+
+?>
---
+++ usrscripts.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userListDiv = document.getElementById("userList");
+});
